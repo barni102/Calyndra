2516df6d52560d0fb6f00c0c9c60ac6cb8963205
barni102 <kovacs102@gmail.com>
2020-11-16 13:04:14 +0100

    Mongo database connected

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 
-const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Server Up and running in: ${port}!`));
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
+    console.log("connected to db")
+);
+
+//Middleware
+app.use(express.json());
+
+app.listen(process.env.SERVER_PORT, () => console.log("Server Up and running in"));
